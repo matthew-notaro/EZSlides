cd8c3b1f4bfbcd3583c9616b91768d16fe56953e
SamanthaLLee <samantha.lin.lee@gmail.com>
2021-04-17 16:52:37 -0400

    formatting

--- bin/analyze.py
+++ bin/analyze.py
@@ -3,19 +3,24 @@ import pytesseract
 import pyperclip
 from PIL import Image   
 
-spam = pyperclip.paste() 
-
 toFile = False
+path = 'bin/test2.jpg'
 
-img = Image.open('bin/test2.jpg')    
-pytesseract.pytesseract.tesseract_cmd ='/usr/local/Cellar/tesseract/4.1.1/bin/tesseract'   
-#pytesseract.pytesseract.tesseract_cmd = r'C:\Users\USER\AppData\Local\Tesseract-OCR\tesseract.exe'
-result = pytesseract.image_to_string(img)   
+def analyzeImage(path):
+    img = Image.open(path)    
+    pytesseract.pytesseract.tesseract_cmd ='/usr/local/Cellar/tesseract/4.1.1/bin/tesseract'   
+    #pytesseract.pytesseract.tesseract_cmd = r'C:\Users\USER\AppData\Local\Tesseract-OCR\tesseract.exe'
 
-if toFile:
-    with open('result.txt', mode ='w') as file:    
-    #with open('result.txt', 'a') as file:      
-        file.write(result)
-        print(result)
-else:
-    pyperclip.copy(result)
+    result = pytesseract.image_to_string(img)   
+    if toFile:
+        with open('result.txt', mode ='w') as file:    
+        #with open('result.txt', 'a') as file:      
+            file.write(result)
+            
+    else:
+        pyperclip.copy(result)
+
+    print(result)
+
+
+analyzeImage(path)
