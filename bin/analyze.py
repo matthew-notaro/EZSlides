cab4183a4a15568e77ff1446b514383b999f26f5
SamanthaLLee <samantha.lin.lee@gmail.com>
2021-04-17 16:48:47 -0400

    clipboard copying works

--- bin/analyze.py
+++ bin/analyze.py
@@ -1,11 +1,21 @@
 import cv2
 import pytesseract
-from PIL import Image    
+import pyperclip
+from PIL import Image   
 
-img = Image.open('bin/test.jpg')     
+spam = pyperclip.paste() 
+
+toFile = False
+
+img = Image.open('bin/test2.jpg')    
 pytesseract.pytesseract.tesseract_cmd ='/usr/local/Cellar/tesseract/4.1.1/bin/tesseract'   
 #pytesseract.pytesseract.tesseract_cmd = r'C:\Users\USER\AppData\Local\Tesseract-OCR\tesseract.exe'
 result = pytesseract.image_to_string(img)   
-with open('result.txt',mode ='w') as file:     
-    file.write(result)
-    print(result)
+
+if toFile:
+    with open('result.txt', mode ='w') as file:    
+    #with open('result.txt', 'a') as file:      
+        file.write(result)
+        print(result)
+else:
+    pyperclip.copy(result)
